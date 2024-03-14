cfe9e5253339febc9bbf8f386d133347276e0656
AmarinoLama <amarinolama@gmail.com>
2024-03-14 12:03:23 +0100

    feat (Brunosbox) : add methods in the main

--- Brunosbox/src/main/java/edu/badpals/Brunosbox/Brunosbox.java
+++ Brunosbox/src/main/java/edu/badpals/Brunosbox/Brunosbox.java
@@ -1,6 +1,8 @@
 package edu.badpals.Brunosbox;
 
+import edu.badpals.Brunosbox.Round.KnockdownRound;
 import edu.badpals.Brunosbox.Round.RegularRound;
+import edu.badpals.Brunosbox.Round.PointsDeducted;
 
 public class Brunosbox
 {
@@ -10,6 +12,16 @@ public class Brunosbox
         round.boxerRoundScore();
         System.out.println("\n\t Regular round\t" + round.getRedBoxerScore() +
                 " - " + round.getBlueBoxerScore());
+
+        KnockdownRound knockdownRound = new KnockdownRound("10 - 8");
+        knockdownRound.boxerRoundScore();
+        System.out.println("\n\t knockdown round\t" + knockdownRound.getRedBoxerScore() +
+                " - " + knockdownRound.getBlueBoxerScore());
+
+        PointsDeducted deducted = new PointsDeducted("10 - 8 ,1");
+        deducted.boxerRoundScore();
+        System.out.println("\n\t points deducted round\t" + deducted.getRedBoxerScore() +
+                " - " + deducted.getBlueBoxerScore());
 
     }
 }
