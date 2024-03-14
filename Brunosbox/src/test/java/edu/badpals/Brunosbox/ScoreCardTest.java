ad7e47f8457fa3235ca270fbc40b3fe6dc88b7a9
AmarinoLama <amarinolama@gmail.com>
2024-03-14 13:29:43 +0100

    test (ScoreCardTest) : create Class

--- Brunosbox/src/test/java/edu/badpals/Brunosbox/ScoreCardTest.java
+++ Brunosbox/src/test/java/edu/badpals/Brunosbox/ScoreCardTest.java
@@ -8,5 +8,6 @@ import static org.junit.Assert.assertEquals;
 public class ScoreCardTest {
     @Test
     public void boxerRoundScoreToIntTest() {
+        /* no soy capaz de hacer test para esta clase */
     }
 }
