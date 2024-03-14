9d931b87f7bff6332b2a4ad6e3c2275b78310e93
AmarinoLama <amarinolama@gmail.com>
2024-03-14 12:02:40 +0100

    feat (RegularRound) : finish class

--- Brunosbox/src/main/java/edu/badpals/Brunosbox/Round/RegularRound.java
+++ Brunosbox/src/main/java/edu/badpals/Brunosbox/Round/RegularRound.java
@@ -19,20 +19,11 @@ public class RegularRound implements Round {
         return this.blueBoxerScore;
     }
 
-    /* no entiendo estos dos de abajo */
-
     @Override
-    public String boxerRoundScore() {
-        String[] parts = this.roundScore.split("-");
-        /*
-        this.blueBoxerScore = (byte) parts[0];
-
-        this.redBoxerScore = (byte) parts[1];
-        */
-        return null;
+    public void boxerRoundScore() {
+        String[] parts = this.roundScore.split(" ");
+        this.blueBoxerScore = (byte) Integer.parseInt(parts[2]);
+        this.redBoxerScore = (byte) Integer.parseInt(parts[0]);
     }
 
-    public RegularRound RegularRound (String roundScore) {
-        return null;
-    }
 }
