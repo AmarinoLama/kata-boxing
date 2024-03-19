0880ac823e922e25b0b7a1f94b8e8fe915a4d70b
AmarinoLama <amarinolama@gmail.com>
2024-03-19 14:25:23 +0100

    refactor (Round) : delete comments

--- Brunosbox/src/main/java/edu/badpals/Brunosbox/Round/Round.java
+++ Brunosbox/src/main/java/edu/badpals/Brunosbox/Round/Round.java
@@ -3,7 +3,5 @@ package edu.badpals.Brunosbox.Round;
 public interface Round {
     byte getRedBoxerScore();
     byte getBlueBoxerScore();
-
-    /* chequear esta de abajo */
     void boxerRoundScore();
 }
