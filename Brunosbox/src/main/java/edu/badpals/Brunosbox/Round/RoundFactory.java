2925a4b8f0c1c394f5f3b8fca2afba9c4064f33c
AmarinoLama <amarinolama@gmail.com>
2024-03-21 09:33:24 +0100

    refactor (RoundFactory) : simplify conditionals

--- Brunosbox/src/main/java/edu/badpals/Brunosbox/Round/RoundFactory.java
+++ Brunosbox/src/main/java/edu/badpals/Brunosbox/Round/RoundFactory.java
@@ -6,17 +6,16 @@ public class RoundFactory {
 
     private Round round;
 
-    public Round getRound(String round) {
-        if (round.length() > 7) {
-            /* es de tipo PointsDeducted */
+    public static Round getRound(String round) {
+        if (round.indexOf(',') != -1) {
             PointsDeducted roundPointsDeducted = new PointsDeducted(round);
             return roundPointsDeducted;
-        } else if (round.replace("8","").length() > 5) {
-            /* es de tipo knockdownround */
-            KnockdownRound roundKnockdown = new KnockdownRound(round);
-            return roundKnockdown;
+        } else {
+            if (round.indexOf('8') != -1) {
+                KnockdownRound roundKnockdown = new KnockdownRound(round);
+                return roundKnockdown;
+            }
         }
-        /* es de tipo regularround */
         RegularRound roundRegular = new RegularRound(round);
         return roundRegular;
     }
